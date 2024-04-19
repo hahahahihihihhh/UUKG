7d59f4b19e2eaeb8a6e0d5208047987ffb48cfac
hahi <1249695730@qq.com>
2024-04-19 23:24:36 +0800

    modify run in USTP_Model

--- USTP_Model/run.py
+++ USTP_Model/run.py
@@ -14,30 +14,30 @@ import json
 
 log = './logs/'
 cache = './libcity/cache/'
-config = './config.json'
+config_file = './config/config_AGCRN_TransE.json'
 
-def init_parser(config):
-    seed = random.randint(0, 10000)
-    parser = argparse.ArgumentParser()
-    # 增加指定的参数
-    parser.add_argument('--task', type=str,
-                        default=config['task'], help='the name of task')
-    parser.add_argument('--model', type=str,
-                        default=config['model'], help='the name of model')
-    parser.add_argument('--dataset', type=str,
-                        default=config['dataset'], help='the name of dataset')
-    parser.add_argument('--config_file', type=str,
-                        default='config', help='the file name of config file')
-    # parser.add_argument('--saved_model', type=str2bool,
-    #                     default=config['saved_model'], help='whether save the trained model')
-    # parser.add_argument('--train', type=str2bool, default=True,
-    #                     help='whether re-train model if the model is trained before')
-    parser.add_argument('--exp_id', type=str, default=str(seed), help='id of experiment')
-    parser.add_argument('--seed', type=int, default=seed, help='random seed')
-    # parser.add_argument('--load_external', type=str2bool, default=True, help="whether to load external data")
-    # 增加其他可选的参数
-    add_general_args(parser)
-    return parser
+# def init_parser(config):
+#     seed = random.randint(0, 10000)
+#     parser = argparse.ArgumentParser()
+#     # 增加指定的参数
+#     parser.add_argument('--task', type=str,
+#                         default=config['task'], help='the name of task')
+#     parser.add_argument('--model', type=str,
+#                         default=config['model'], help='the name of model')
+#     parser.add_argument('--dataset', type=str,
+#                         default=config['dataset'], help='the name of dataset')
+#     parser.add_argument('--config_file', type=str,
+#                         default=config_file, help='the file name of config file')
+#     # parser.add_argument('--saved_model', type=str2bool,
+#     #                     default=config['saved_model'], help='whether save the trained model')
+#     # parser.add_argument('--train', type=str2bool, default=True,
+#     #                     help='whether re-train model if the model is trained before')
+#     parser.add_argument('--exp_id', type=str, default=str(seed), help='id of experiment')
+#     parser.add_argument('--seed', type=int, default=seed, help='random seed')
+#     # parser.add_argument('--load_external', type=str2bool, default=True, help="whether to load external data")
+#     # 增加其他可选的参数
+#     add_general_args(parser)
+#     return parser
 
 def train(config, total = 5):
     modelName, datasetName, KGE = config['model'], config['dataset'], config['load_external']
@@ -45,7 +45,7 @@ def train(config, total = 5):
     save_dir = log + datasetName[0:3] + '/' + modelName + '/' + datasetName + ('_KGE' if KGE else '') + '/'
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    exp_ids, eval_metrics = [], ["MAE", "RMSE"]
+    exp_ids, eval_metrics = [], config["metrics"]    # ['MAE', 'RMSE']
     final_results_five_train = np.zeros([predict_steps, len(eval_metrics), total])
 
     # file logger
@@ -56,34 +56,26 @@ def train(config, total = 5):
         filename=os.path.join(save_dir, "train.log"),
         filemode='w'
     )
-
-    # stdout logger
-    # console = logging.StreamHandler()
-    # console.setLevel(logging.INFO)
-    # formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
-    # console.setFormatter(formatter)
-    # logging.getLogger("").addHandler(console)
-    # logging.info("Saving logs in: {}".format(save_dir))
-
+    original_seed = config['seed']
     for _i in range(total):
         # 解析参数
-        parser = init_parser(config)
-        args = parser.parse_args()
-        dict_args = vars(args)
-        print(dict_args)
-        exp_id = dict_args['exp_id']
-        exp_ids.append(exp_id)
+        # parser = init_parser(config)
+        # args = parser.parse_args()
+        # dict_args = vars(args)
+        config['seed'] = config['exp_id'] = original_seed + _i
+        # exp_id = dict_args['exp_id']
+        exp_ids.append(config['exp_id'])
         logging.info('----------------------------------------------------------------------------')
-        logging.info("# {}, Kownledge Graph Embedding: {}, Experiment ids: {}".format(_i, KGE, exp_id))
+        logging.info("# {}, Kownledge Graph Embedding: {}, Experiment ids: {}".format(_i, KGE, config['exp_id']))
         logging.info('----------------------------------------------------------------------------')
-        other_args = {key: val for key, val in dict_args.items() if key not in [
+        other_args = {key: val for key, val in config.items() if key not in [
             'task', 'model', 'dataset', 'config_file', 'saved_model', 'train'] and
             val is not None}
         # 运行模型
-        run_model(task=args.task, model_name=args.model, dataset_name=args.dataset,
-                  config_file=args.config_file, other_args=other_args)
+        run_model(task=config['task'], model_name=config['model'], dataset_name=config['dataset'],
+                 other_args=other_args)
         # 获取指标
-        result = pd.read_csv(cache + exp_id + '/' + 'evaluate_cache/' + modelName + '_' + datasetName + '.csv')
+        result = pd.read_csv(cache + str(config['exp_id']) + '/' + 'evaluate_cache/' + modelName + '_' + datasetName + '.csv')
         temp = result[eval_metrics].values
         final_results_five_train[:, :, _i] = temp
     logging.info('----------------------------------------------------------------------------')
@@ -122,6 +114,7 @@ def train(config, total = 5):
     logging.info('{} steps Standard deviation: \n{}'.format(predict_steps, df_std_steps_results))
 
 if __name__ == '__main__':
-    with open(config) as config:
+    with open(config_file) as config:
         config = json.load(config)
+        print(config)
         train(config)
